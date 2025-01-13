7943c2e11bd1e685028d543687e8a225d0a1b5d0
Grant <grantdav110@gmail.com>
2025-01-13 14:01:48 -0600

    Fixes

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,16 +1,18 @@
 import Card from '../components/Card'
+import ponggif from '../assets/ponggif.gif'
+import sortinggif from '../assets/sortinggif.gif'
 
 export const Projects = () => {
   return (
     <div className='cardContainer'>
       <Card title="Pong" 
-            imgPath='src\assets\ponggif.gif'
+            imgPath={ponggif}
             bodyText='A project made to improve my C++ skills, where I recreated pong.
              Tools: C++/SDL2'
             redirect='https://github.com/TheatricalGrant/RetroGamesRecreated'
             />
       <Card title="Sorting Algorithm Visualizer" 
-            imgPath='src\assets\sortinggif.gif'
+            imgPath={sortinggif}
             bodyText='A project made to expand my algorithm knowledge.
              Tools: C++/SDL2'
             redirect='https://github.com/TheatricalGrant/SimpleSortVisualizer'
