d3d8c39ee8915d11c175fa82a17d00b07f276c7d
Grant <grantdav110@gmail.com>
2025-01-13 14:09:18 -0600

    final

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,6 @@
 import Card from '../components/Card'
-import ponggif from 'theatricalgrant.github.io/src/assets/ponggif.gif'
-import sortinggif from 'theatricalgrant.github.io/src/assets/sortinggif.gif'
+import ponggif from 'https://github.com/TheatricalGrant/theatricalgrant.github.io/blob/main/src/assets/ponggif.gif'
+import sortinggif from 'https://github.com/TheatricalGrant/theatricalgrant.github.io/blob/main/src/assets/sortinggif.gif'
 
 export const Projects = () => {
   return (
